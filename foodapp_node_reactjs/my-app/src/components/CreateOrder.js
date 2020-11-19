637265e744ac954b08e09be93b1847087992b550
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-11-19 21:34:34 +0530

    added frontend for food app

---
+++ foodapp_node_reactjs/my-app/src/components/CreateOrder.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+const CreateOrder = ({onChangeForm, createOrder }) => {
+
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-7 mrgnbtm">
+                <h2 style={{ marginBottom: 40 }}>Create Order</h2>
+                <form>
+                    <div className="row">
+                        <div className="form-group col-md-6">
+                        <label>Select food item</label>
+                            <select 
+                            onChange={(e) => onChangeForm(e)}
+                            className="form-control" 
+                            name="item" id="item" placeholder="Select food item"
+                            >
+                                <option value="item">option</option>
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                </select> 
+                        </div>
+                        <div className="form-group col-md-6">
+                        <label>Quantity</label>
+                            <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" name="quantity" id="quantity" placeholder="Quantity" autoComplete="off"/>
+                        </div>
+                    </div>
+                    
+                    <button type="button" onClick= {(e) => createOrder()} className="btn btn-success orderButton">Order Now</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateOrder
